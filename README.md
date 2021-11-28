5c02388efe5bc1fbdb81f507c137e1b789a22342
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-11-28 17:27:18 +0300

    Add -y flag for year progress

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Print day progress in your terminal
 ```bash
 go install github.com/tsivinsky/day-progress@latest
 ```
+
+## Usage
+
+```bash
+day-progress
+```
+
+By default, day-progress will show only day progress but you can add flags to customize its behavior
+
+#### Show year progress
+
+```bash
+day-progress -y
+```
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
 	"time"
 )
 
+var (
+	showYear bool
+)
+
 func main() {
+	flag.BoolVar(&showYear, "y", false, "day-progress -y")
+
+	flag.Parse()
+
 	printDayProgress()
+
+	if showYear {
+		printYearProgress()
+	}
+}
+
+func printYearProgress() {
+	dayOfYear := time.Now().YearDay()
+	daysInYear := 365
+
+	percentage := int(math.Floor(float64(dayOfYear) / float64(daysInYear) * 100))
+
+	fmt.Print("Year: [")
+
+	for i := 0; i < 100; i += 10 {
+		fmt.Print("#")
+	}
+
+	fmt.Printf("] %d%%\n", percentage)
 }
 
 func printDayProgress() {
@@ -17,7 +45,11 @@ func printDayProgress() {
 	secondsInDay := 86400
 	percentage := int(math.Round(offset / float64(secondsInDay) * 100))
 
-	fmt.Print("Day: [")
+	if showYear {
+		fmt.Print("Day:  [")
+	} else {
+		fmt.Print("Day: [")
+	}
 
 	for i := 0; i < 100; i += 10 {
 		if i < percentage {
